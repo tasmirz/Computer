cd04d3be79354e0300ea257dcd1ba5e0523c5ed2
kuetcse <cse.kuet@protonmail.com>
2024-03-22 05:33:15 +0600

    relayout

--- compiler.cpp
+++ compiler.cpp
@@ -9,7 +9,14 @@ int main(int argc, char* argv[]) {
   /*
   expand complex instructions
   into assembly
-  find location of labels remove labels
-  use deque
+  do not push labels
+  use vector
   */
+  string line;
+  int current = 0;
+  while (std::getline(source, line)) {
+    std::istringstream ss(line);
+    // Now you can use the ss object to process the current line
+    // ... your code to process the line using ss ...
+  }
 }
